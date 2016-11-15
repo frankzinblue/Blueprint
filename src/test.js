edaedf184cccaecda9fc1024cabd0e0a458b0018
Franklin Zhang <frankzinblue@gmail.com>
2016-11-15 15:22:44 -0500

    polish variable naming

--- src/test.js
+++ src/test.js
@@ -1,47 +1,53 @@
-var button = document.getElementById("mybutton");
-var testDisplay = document.getElementById("testDisplay");
-var timer;
-var endDate;
-var displayId;
+(function () {
 
-button.addEventListener("click", function() {
-    var testInput = document.getElementById('testInput').value;
-    startFunction(testInput, 'countdown')
-});
+    var button = document.getElementById("mybutton");
+    var testDisplay = document.getElementById("testDisplay");
+    var timer;
+    var endDate;
+    var targetDisplayId;
 
-function startFunction (dt, id) {
-    endDate = new Date(dt);
-    displayId = id;
-    stopFunction();
-    timer = setInterval(function () {
-        showRemaining()
-    }, 1000);
-}
+    button.addEventListener("click", function() {
+        var testInput = document.getElementById('testInput').value;
+        startFunction(testInput, 'countdown')
+    });
 
-function stopFunction () {
-    clearInterval(timer);
-}
-
-function showRemaining () {
-    var second = 1000;
-    var minute = second * 60;
-    var hour = minute * 60;
-    var day = hour * 24;
-    var now = new Date();
-    var distance = endDate - now;
-    if (distance < 0) {
-        clearInterval(timer);
-        document.getElementById(displayId).innerHTML = 'Expired!';
-        return;
+    function startFunction (endDateInput, id) {
+        endDate = new Date(endDateInput);
+        targetDisplayId = id;
+        stopFunction();
+        timer = setInterval(function () {
+            showRemaining()
+        }, 1000);
     }
 
-    var days = Math.floor(distance / day);
-    var hours = Math.floor((distance % day) / hour);
-    var minutes = Math.floor((distance % hour) / minute);
-    var seconds = Math.floor((distance % minute) / second);
+    function stopFunction () {
+        clearInterval(timer);
+    }
 
-    document.getElementById(displayId).innerHTML = days + 'days ';
-    document.getElementById(displayId).innerHTML += hours + 'hrs ';
-    document.getElementById(displayId).innerHTML += minutes + 'mins ';
-    document.getElementById(displayId).innerHTML += seconds + 'secs ';
-}
+    function showRemaining () {
+        var second = 1000,
+            minute = second * 60,
+            hour = minute * 60,
+            day = hour * 24;
+
+        var now = new Date();
+        var distance = endDate - now;
+
+        if (distance < 0) {
+            clearInterval(timer);
+            document.getElementById(targetDisplayId).innerHTML = 'Expired!';
+            return;
+        }
+
+        var days = Math.floor(distance / day);
+        var hours = Math.floor((distance % day) / hour);
+        var minutes = Math.floor((distance % hour) / minute);
+        var seconds = Math.floor((distance % minute) / second);
+
+        document.getElementById(targetDisplayId).innerHTML = days + 'days ';
+        document.getElementById(targetDisplayId).innerHTML += hours + 'hrs ';
+        document.getElementById(targetDisplayId).innerHTML += minutes + 'mins ';
+        document.getElementById(targetDisplayId).innerHTML += seconds + 'secs ';
+    }
+
+})();
