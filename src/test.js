900e9aaab34c2662822a0e1af00d2d22fe5a5fc9
Franklin Zhang <frankzinblue@gmail.com>
2016-11-15 15:10:08 -0500

    fix login flaw

--- src/test.js
+++ src/test.js
@@ -1,39 +1,47 @@
 var button = document.getElementById("mybutton");
 var testDisplay = document.getElementById("testDisplay");
+var timer;
+var endDate;
+var displayId;
 
 button.addEventListener("click", function() {
     var testInput = document.getElementById('testInput').value;
-    // testDisplay.innerHTML = testInput;
-    countDownTimer(testInput, 'countdown')
+    startFunction(testInput, 'countdown')
 });
 
-function countDownTimer (dt, id) {
-    var end = new Date(dt);
+function startFunction (dt, id) {
+    endDate = new Date(dt);
+    displayId = id;
+    stopFunction();
+    timer = setInterval(function () {
+        showRemaining()
+    }, 1000);
+}
+
+function stopFunction () {
+    clearInterval(timer);
+}
+
+function showRemaining () {
     var second = 1000;
     var minute = second * 60;
     var hour = minute * 60;
     var day = hour * 24;
-    var timer;
-
-    function showRemaining () {
-        var now = new Date();
-        var distance = end - now;
-        if (distance < 0) {
-            clearInterval(timer);
-            document.getElementById(id).innerHTML = 'Expired!';
-            return;
-        }
-
-        var days = Math.floor(distance / day);
-        var hours = Math.floor((distance % day) / hour);
-        var minutes = Math.floor((distance % hour) / minute);
-        var seconds = Math.floor((distance % minute) / second);
-
-        document.getElementById(id).innerHTML = days + 'days ';
-        document.getElementById(id).innerHTML += hours + 'hrs ';
-        document.getElementById(id).innerHTML += minutes + 'mins ';
-        document.getElementById(id).innerHTML += seconds + 'secs ';
+    var now = new Date();
+    var distance = endDate - now;
+    if (distance < 0) {
+        clearInterval(timer);
+        document.getElementById(displayId).innerHTML = 'Expired!';
+        return;
     }
 
-    timer = setInterval(showRemaining, 1000);
+    var days = Math.floor(distance / day);
+    var hours = Math.floor((distance % day) / hour);
+    var minutes = Math.floor((distance % hour) / minute);
+    var seconds = Math.floor((distance % minute) / second);
+
+    document.getElementById(displayId).innerHTML = days + 'days ';
+    document.getElementById(displayId).innerHTML += hours + 'hrs ';
+    document.getElementById(displayId).innerHTML += minutes + 'mins ';
+    document.getElementById(displayId).innerHTML += seconds + 'secs ';
 }
